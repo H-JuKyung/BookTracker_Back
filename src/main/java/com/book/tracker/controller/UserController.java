ee71806f04506bed22b9a5ca3bf67ec4d9a78ff9
H-JuKyung <hjk000hjk17@gmail.com>
2025-03-17 00:54:04 +0900

    feat: 회원가입, 로그인 기능 구현 완료(token 사용)

--- src/main/java/com/book/tracker/controller/UserController.java
+++ src/main/java/com/book/tracker/controller/UserController.java
@@ -5,23 +5,86 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.book.tracker.dto.Login;
 import com.book.tracker.dto.User;
 import com.book.tracker.service.UserService;
 
 @RestController
-@CrossOrigin("http://127.0.0.1:5501/")
+@CrossOrigin("http://127.0.0.1:5500/")
 public class UserController {
 
 	@Autowired
 	UserService userService;
 	
+	@PostMapping("logout")
+	public void logout(@RequestHeader String authorization) {
+		System.out.println(authorization);
+		try {
+			userService.logout(authorization);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@PostMapping("tokenLogin")
+	public Map<String,String> tokenLogin(@RequestBody User u) {
+		System.out.println(u);
+		
+		Map<String,String> responseMap = new HashMap<>();
+		
+		try {
+			Login loginInfo = userService.tokenLogin(u);
+			
+			if(loginInfo != null && loginInfo.getNickname() != null && loginInfo.getToken() != null) {
+                responseMap.put("msg", "ok");
+				responseMap.put("nickname", loginInfo.getNickname());
+				responseMap.put("Authorization", loginInfo.getToken());
+			}else {
+				responseMap.put("msg", "다시 로그인 해주세요");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			responseMap.put("msg", "다시 로그인 해주세요");
+		}
+		return responseMap;
+	}
+	
+//	@PostMapping("login")
+//	public Map<String,String> login(@RequestBody User u) {
+//		System.out.println(u);
+//		
+//		Map<String,String> responseMap = new HashMap<>();
+//		
+//		try {
+//			u = userService.login(u);
+//			String nickname = u.getNickname();
+//			if(u != null && nickname != null && !nickname.trim().equals("")) {
+//				responseMap.put("nickname", nickname);
+//			}else {
+//				responseMap.put("msg", "다시 로그인 해주세요");
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			responseMap.put("msg", "다시 로그인 해주세요");
+//		}
+//		return responseMap;
+//	}
+	
 	@PostMapping("insertUser")
-	public Map<String, String> insertUser(@RequestBody  User u) {
-		Map<String,String> responseData=new HashMap();
+	public Map<String,String> insertUser(@RequestBody User u) {
+		System.out.println(u);
+		
+		Map<String,String> responseData = new HashMap<>();
 		try {
 			userService.insertUser(u);
 			responseData.put("msg","ok");
@@ -30,7 +93,45 @@ public class UserController {
 			e.printStackTrace();
 			responseData.put("msg",e.getMessage());
 		}
-		
+
 		return responseData;
+	}
+	
+	@PostMapping("updateUser")
+	public String updateUser(@RequestBody User u) {
+		System.out.println(u);
+		try {
+			userService.updateUser(u);
+			return "ok";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "email과 pwd 확인해 주세요";
+		}
+	}
+	
+	@PostMapping("deleteUser")
+	public String deleteUser(@RequestBody String email) {
+		System.out.println(email);
+		try {
+			userService.deleteUser(email);
+			return "ok";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "email과 pwd 확인해 주세요";
+		}
+	}
+	
+	@GetMapping("checkNickname")
+	public Map<String, Boolean> checkNickname(@RequestParam String nickname) {
+	    System.out.println("닉네임 중복 확인 요청: " + nickname);
+	    
+	    boolean isAvailable = userService.isNicknameAvailable(nickname);
+	    
+	    Map<String, Boolean> response = new HashMap<>();
+	    response.put("available", isAvailable);
+	    
+	    return response;
 	}
 }
--- src/main/java/com/book/tracker/service/UserService.java
+++ src/main/java/com/book/tracker/service/UserService.java
@@ -1,10 +1,19 @@
 package com.book.tracker.service;
 
+import java.util.Date; 
+import java.util.UUID;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.book.tracker.dao.LoginDao;
+import com.book.tracker.dao.SaltDao;
 import com.book.tracker.dao.UserDao;
+import com.book.tracker.dto.Login;
+import com.book.tracker.dto.SaltInfo;
 import com.book.tracker.dto.User;
+import com.book.tracker.util.OpenCrypt;
 
 @Service
 public class UserService {
@@ -12,7 +21,114 @@ public class UserService {
 	@Autowired
 	UserDao userDao;
 	
-	public void insertUser(User u) throws Exception {
+	@Autowired
+	LoginDao loginDao;
+	
+	@Autowired
+	SaltDao saltDao;
+	
+	public Login checkToken(String authorization) throws Exception {
+		// TODO Auto-generated method stub
+		return loginDao.checkToken(authorization);
+	}
+	
+	public Login tokenLogin(User u) throws Exception {
+		String email = u.getEmail();
+		//email로 salt를 찾아옴
+		SaltInfo saltInfo = saltDao.selectSalt(email);
+		//pwd에 salt를 더하여 암호화
+		String pwd = u.getPwd();
+		byte [] pwdHash = OpenCrypt.getSHA256(pwd, saltInfo.getSalt());
+		String pwdHashHex = OpenCrypt.byteArrayToHex(pwdHash);
+		u.setPwd(pwdHashHex);
+		u = userDao.login(u);		
+		
+		if(u != null) {
+			String nickname = u.getNickname();
+			if(nickname != null && !nickname.trim().equals("")) {
+				//member table에서 email과 pwd가 확인된 상황 즉, login ok				
+				
+				//1. salt를 생성한다
+				String salt = UUID.randomUUID().toString();
+				System.out.println("salt: " + salt);
+				//2. email을 hashing 한다
+				byte[] originalHash = OpenCrypt.getSHA256(email, salt);
+				//3. db에 저장하기 좋은 포맷으로 인코딩한다
+				String myToken = OpenCrypt.byteArrayToHex(originalHash);
+				System.out.println("myToken: " + myToken);
+				
+				//4. login table에 token 저장
+				Login loginInfo = new Login(email, myToken, nickname, new Date());
+				loginDao.insertToken(loginInfo);
+				return loginInfo;
+			}
+		}
+		
+		return null;		 
+	}
+	
+//	public User login(User u) throws Exception {
+//	    return userDao.login(u);
+//	}
+	
+	public void insertUser(User u) throws Exception{
+		
+		// 이메일 유효성 검사
+		String email = u.getEmail();
+	    if (!isValidEmail(email)) {
+	        throw new Exception("유효하지 않은 이메일 형식입니다.");
+	    }		
+		
+		// 패스워드 유효성 검사
+	    String pwd = u.getPwd();
+	    if (!isValidPassword(pwd)) {
+	        throw new Exception("패스워드는 8자리 이상이어야 하며, 특수문자와 숫자를 포함해야 합니다.");
+	    }
+	    
+	    // 패스워드 암호화
+	    //1. salt를 생성한다
+		String salt = UUID.randomUUID().toString();
+		System.out.println("salt: " + salt);
+		//2. pwd를 hashing 한다
+		byte[] originalHash = OpenCrypt.getSHA256(pwd, salt);
+		//3. db에 저장하기 좋은 포맷으로 인코딩한다
+		String pwdHash = OpenCrypt.byteArrayToHex(originalHash);
+		System.out.println("pwdHash: " + pwdHash);
+	    
+		u.setPwd(pwdHash);
+	    
+		saltDao.insertSalt(new SaltInfo(email, salt));
 		userDao.insertUser(u);
+	}
+	
+	public void updateUser(User u) throws Exception{
+		userDao.updateUser(u);
+	}
+	
+	public void deleteUser(String email) throws Exception{
+		userDao.deleteUser(email);
+	}
+
+	public void logout(String authorization) throws Exception {
+		loginDao.deleteToken(authorization);
+	}
+	
+	public boolean isNicknameAvailable(String nickname) {
+		User existingUser = userDao.useByNickname(nickname);
+		return existingUser == null; // 닉네임이 존재하지 않으면 true (사용 가능)
+	}
+	
+	// 이메일 유효성 검사 메서드
+	private boolean isValidEmail(String email) {
+	    // 이메일 패턴
+	    String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+	    return Pattern.matches(emailPattern, email);
+	}
+	
+	// 패스워드 유효성 검사 메서드
+	private boolean isValidPassword(String password) {
+	    // 패스워드 패턴: 8자리 이상, 숫자 포함, 특수문자 포함
+	    String passwordPattern = "^(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$";
+	    return Pattern.matches(passwordPattern, password);
 	}
 }
